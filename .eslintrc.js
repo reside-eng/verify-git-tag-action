74379b39b6c09a17020f8972c679e27c0bfec347
Eddie Leffler <757524+hershmire@users.noreply.github.com>
2020-06-24 13:59:39 -0600

    feat: wip typescript error

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,12 @@ module.exports = {
     '@side/prettier',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
+    'plugin:jsdoc/recommended',
   ],
   parser: '@typescript-eslint/parser',
+  rules: {
+    'no-console': 0,
+    'jsdoc/require-returns-type': 0,
+    'jsdoc/require-param-type': 0,
+  },
 };
