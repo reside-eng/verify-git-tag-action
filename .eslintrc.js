fcaa43012dd47baa0faad82a20c75ccc0ead03db
Eddie Leffler <757524+hershmire@users.noreply.github.com>
2020-06-24 12:32:32 -0600

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    '@side/base',
+    '@side/prettier',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+};
